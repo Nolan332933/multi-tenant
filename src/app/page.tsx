1b7c70f2dc1b821c7d539d3e54761b5a9c5a4962
KaungMyatKywal <nolanshelby81@gmail.com>
2025-06-10 16:54:32 +0630

    customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
 import { Button } from "@/components/ui/button"
-
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Progress } from "@/components/ui/progress"
 const page = () => {
   return (
-    <div>
-      <Button className="bg-red-500">Click Me</Button>
-      <p className="text-rose-500 ">Hi there</p>
+    <div className="flex flex-col gap-y-4 ">
+      <div>
+       <Button variant="elevated">i am a button</Button>
+      </div>
+      <div>
+        <Input placeholder="I am an input"/>
+      </div>
+      <div>
+        <Progress value={50}/>
+      </div>
+      <div>
+        <Textarea placeholder="I am a text"/>
+      </div>
+      <div>
+        <input type="checkbox" />
+      </div>
     </div>
   )
 }
